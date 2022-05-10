2fe5a35e3d310bf8af342d61ece158e7e61497f6
Abdellah Dbaich <adbaich@e2r12p5.1337.ma>
2022-05-10 19:39:25 +0100

    jinx

--- philosophers.c
+++ philosophers.c
@@ -6,97 +6,23 @@
 /*   By: adbaich <adbaich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 10:19:30 by adbaich           #+#    #+#             */
-/*   Updated: 2022/05/07 13:34:40 by adbaich          ###   ########.fr       */
+/*   Updated: 2022/05/10 19:32:37 by adbaich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-size_t	get_time(void)
-{
-	size_t				time;
-	struct timeval	current_time;
-	
-	gettimeofday(&current_time, NULL);
-	time = (current_time.tv_sec * 1000) + (current_time.tv_usec / 1000);
-	return (time);
-}
-
-size_t	get_usleep_time(size_t time_action)
-{
-	size_t	usleep_time;
-
-	usleep_time = 10 * time_action;
-	return (usleep_time);
-}
-
-size_t	get_current_time_philo(t_vars *philo)
-{
-	size_t current_time;
-
-	current_time = get_time() - philo->init_time;
-	return (current_time);
-}
-
-void	usleep_action(size_t time_action, t_vars *philo)
-{
-	time_action = time_action + get_current_time_philo(philo);
-	while (get_current_time_philo(philo) != time_action)
-		usleep(400);
-}
-
-void	think_time(t_vars *philo)
-{
-	// if (philo->index == 1)
-	// 	printf("jgdhjsgfsilailahailaallaj\n");
-	printf("%zu ms %d is thinking\n", get_current_time_philo(philo), philo->index);
-	usleep_action(2, philo);
-	eat_time((void *)philo);
-}
-
-void	sleep_time(t_vars *philo)
-{
-	printf("%zu ms %d is sleeping\n", get_current_time_philo(philo), philo->index);
-	usleep_action(philo->time_to_sleep, philo);
-	think_time(philo);
-}
-
-void	*eat_time(void *philo1)
-{
-	size_t	limit_time;
-	size_t	usleep_time;
-	t_vars 	*philo;
-	size_t	i;
-
-
-	philo = (t_vars *)philo1;
-	pthread_mutex_lock(&philo->mutex[philo->index - 1]);
-	printf("%zu ms %d has taken a fork\n", get_current_time_philo(philo), philo->index);
-	if (philo->number_of_philosophers == philo->index)
-		pthread_mutex_lock(&philo->mutex[0]);
-	else
-		pthread_mutex_lock(&philo->mutex[philo->index]);
-	printf("%zu ms %d has taken a fork\n", get_current_time_philo(philo), philo->index);
-	printf("%zu ms %d is eating\n", get_current_time_philo(philo), philo->index);
-	philo->last_meal = get_current_time_philo(philo);
-	usleep_action(philo->time_to_eat, philo);
-	pthread_mutex_unlock(&philo->mutex[philo->index - 1]);
-	if (philo->number_of_philosophers == philo->index)
-		pthread_mutex_unlock(&philo->mutex[0]);
-	else
-		pthread_mutex_unlock(&philo->mutex[philo->index]);
-	sleep_time(philo);
-	return (philo1);
-}
-
 void	create_philosophers(c_vars *common_info)
 {
+	pthread_mutex_t	msg;
 	pthread_t		*t;
-	pthread_mutex_t *mutex;
+	pthread_mutex_t	*mutex;
 	t_vars			*philo;
+	size_t			last_meal;
 	int				i;
+	int				j;
+	int				num_of_philos_ate;
 	int				index;
-
 
 	t = malloc(sizeof(pthread_t) * common_info->number_of_philosophers);
 	if (!t)
@@ -104,9 +30,11 @@ void	create_philosophers(c_vars *common_info)
 	philo = malloc(sizeof(t_vars) * common_info->number_of_philosophers);
 	if (!philo)
 		return ;
-	mutex = malloc(sizeof(pthread_mutex_t) * common_info->number_of_philosophers);
+	mutex = malloc(sizeof(pthread_mutex_t)
+			* common_info->number_of_philosophers);
 	if (!mutex)
 		return ;
+	pthread_mutex_init(&msg, NULL);
 	i = 0;
 	while (i < common_info->number_of_philosophers)
 	{
@@ -117,6 +45,7 @@ void	create_philosophers(c_vars *common_info)
 	while (i < common_info->number_of_philosophers)
 	{
 		philo[i].init_time = common_info->init_time;
+		philo[i].msg = &msg;
 		philo[i].last_meal = 0;
 		philo[i].mutex = mutex;
 		philo[i].number_of_philosophers = common_info->number_of_philosophers;
@@ -124,6 +53,7 @@ void	create_philosophers(c_vars *common_info)
 		philo[i].time_to_die = common_info->time_to_die;
 		philo[i].time_to_eat = common_info->time_to_eat;
 		philo[i].time_to_sleep = common_info->time_to_sleep;
+		philo[i].time_must_eat = common_info->time_must_eat;
 		philo[i].index = i + 1;
 		pthread_create(&t[i], NULL, eat_time, (void *)&philo[i]);
 		usleep(100);
@@ -131,10 +61,24 @@ void	create_philosophers(c_vars *common_info)
 	}
 	while (1)
 	{
+		if (common_info->time_must_eat > 0)
+		{
+			j = 0;
+			num_of_philos_ate = 0;
+			while (j < common_info->number_of_philosophers)
+			{
+				if (philo[j].time_must_eat == 0)
+					num_of_philos_ate++;
+				j++;
+			}
+			if (num_of_philos_ate == common_info->number_of_philosophers)
+				return ;
+		}
 		i = 0;
 		while (i < common_info->number_of_philosophers)
 		{
-			if (get_current_time_philo(&philo[i]) - philo[i].last_meal > common_info->time_to_die)
+			last_meal = get_current_time_philo(&philo[i]) - philo[i].last_meal;
+			if (last_meal > common_info->time_to_die)
 			{
 				index = i;
 				i = 0;
@@ -143,23 +87,57 @@ void	create_philosophers(c_vars *common_info)
 					pthread_mutex_destroy(&mutex[i]);
 					i++;
 				}
-				printf("%zu ms %d died\n", get_current_time_philo(&philo[index]), philo[index].index);
+				send_msg(0, &philo[index]);
+				pthread_mutex_destroy(&msg);
 				return ;
 			}
 			i++;
 		}
+		usleep(100);
 	}
 }
 
 int	main(int ac, char **av)
 {
 	c_vars	common_info;
+	int		count;
 
-	common_info.number_of_philosophers = ft_atoi(av[1]);
-	common_info.time_to_die = ft_atoi(av[2]);
-	common_info.time_to_eat = ft_atoi(av[3]);
-	common_info.time_to_sleep = ft_atoi(av[4]);
-	common_info.init_time = get_time();
-	create_philosophers(&common_info);
+	count = 1;
+	if (ac == 5)
+	{
+		while (check_args_help(av[count]) && count < 4)
+			count++;
+		if (!check_args_help(av[count]))
+			count--;
+		if (check_args1(ft_atoi(av[2]), ft_atoi(av[1]),
+				ft_atoi(av[3])) && check_args2(ft_atoi(av[4]), 1) && count == 4)
+		{
+			common_info.number_of_philosophers = ft_atoi(av[1]);
+			common_info.time_to_die = ft_atoi(av[2]);
+			common_info.time_to_eat = ft_atoi(av[3]);
+			common_info.time_to_sleep = ft_atoi(av[4]);
+			common_info.time_must_eat = -2;
+			common_info.init_time = get_time();
+			create_philosophers(&common_info);
+		}
+	}
+	else if (ac == 6)
+	{
+		while (check_args_help(av[count]) && count < 5)
+			count++;
+		if (!check_args_help(av[count]))
+			count--;
+		if (check_args1(ft_atoi(av[2]), ft_atoi(av[1]), ft_atoi(av[3]))
+			&& check_args2(ft_atoi(av[4]), ft_atoi(av[5])) && count == 5)
+		{
+			common_info.number_of_philosophers = ft_atoi(av[1]);
+			common_info.time_to_die = ft_atoi(av[2]);
+			common_info.time_to_eat = ft_atoi(av[3]);
+			common_info.time_to_sleep = ft_atoi(av[4]);
+			common_info.time_must_eat = ft_atoi(av[5]);
+			common_info.init_time = get_time();
+			create_philosophers(&common_info);
+		}
+	}
 	return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: adbaich <adbaich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 10:19:38 by adbaich           #+#    #+#             */
-/*   Updated: 2022/04/26 23:14:50 by adbaich          ###   ########.fr       */
+/*   Updated: 2022/05/10 19:32:26 by adbaich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,37 @@
 #include <stdlib.h>
 typedef struct s_vars
 {
-	int		number_of_philosophers;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		index;
-	size_t	init_time;
+	int				number_of_philosophers;
+	size_t			time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				time_must_eat;
+	int				index;
+	size_t			init_time;
+	size_t			last_meal;
 	pthread_mutex_t *mutex;
-	size_t	last_meal;
+	pthread_mutex_t *msg;
 } t_vars;
 
 typedef struct i_vars
 {
-	int		number_of_philosophers;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	size_t	init_time;
+	int			number_of_philosophers;
+	int			time_to_eat;
+	int			time_must_eat;
+	size_t		time_to_die;
+	int			time_to_sleep;
+	size_t		init_time;
 } c_vars;
 
 int		ft_atoi(const char *str);
 void	*eat_time(void *philo1);
+void	send_msg(int action_index, t_vars *philo);
+size_t	get_time(void);
+size_t	get_current_time_philo(t_vars *philo);
+void	usleep_action(size_t time_action, t_vars *philo);
+void	*eat_time(void *philo1);
+void	sleep_time(t_vars *philo);
+void	think_time(t_vars *philo);
+int		check_args_help(char *p);
+int		check_args1(int a, int b, int c);
+int		check_args2(int d, int e);
